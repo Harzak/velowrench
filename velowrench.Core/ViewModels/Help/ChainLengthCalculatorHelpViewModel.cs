b7fa0f3be2bc8a7fb73191a328323961273155b7
aurelien-aubry <35953576+Harzak@users.noreply.github.com>
2025-08-09 21:24:20 +0200

    Add help page support for chain length calculator

--- velowrench.Core/ViewModels/Help/ChainLengthCalculatorHelpViewModel.cs
+++ velowrench.Core/ViewModels/Help/ChainLengthCalculatorHelpViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,9 +9,15 @@ using velowrench.Core.ViewModels.Base;
 
 namespace velowrench.Core.ViewModels.Help;
 
-public class ChainLengthCalculatorHelpViewModel : BaseRoutableViewModel
+public partial class ChainLengthCalculatorHelpViewModel : BaseRoutableViewModel
 {
     public override string Name => "Chain length Help";
+
+    [ObservableProperty]
+    private string _simpleEquationLaTeX = @"L = 2 \cdot C + \frac{F}{4} + \frac{R}{4} + 1";
+
+    [ObservableProperty]
+    private string _rigorousEquationLaTeX = @"L = 0.25 \cdot \left(F + R\right) + 2 \cdot \sqrt{C^{2} + \left(0.0796 \cdot \left(F - R\right)\right)^{2}}";
 
     public ChainLengthCalculatorHelpViewModel(INavigationService navigationService, ILocalizer  localizer) : base(navigationService)
     {
